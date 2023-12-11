3798eff4d77cb12b8e15bbcc2f280929179d13fd
StephanyGarcesGuerra <stephanygarces96@gmail.com>
2023-12-11 14:59:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./database.js";
+
+async function getUserData(id) {
+    try{ 
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+  const db = await central(id);
+  const[info, secure]= await Promise.all([dbs[db], vault(id)]);
+
+  return{id, ...info, ...secure};
+ } catch(error){
+    return error;
+  }
+};
+
+(async() => {
+    const time = new Date().getTime();
+    console.log(await getUserData(1));
+    console.log("processed completed in:", new Date().getTime()-time, "ms");
+})(); 
+
+
